bca4c8dcd0615851205281b307f6ddf23d2486d3
frezadev <freza.dev@gmail.com>
2016-02-29 14:53:53 +0700

    add filebrowser.go for file struct

---
+++ v0/filebrowser.go
@@ -0,0 +1,27 @@
+package colonycore
+
+import (
+	// "github.com/eaciit/orm/v1"
+	"github.com/eaciit/toolkit"
+	"time"
+)
+
+type FileInfo struct {
+	Name         string        `json:"name", bson:"name"`
+	Size         float64       `json:"size", bson:"size"`
+	User         string        `json:"user", bson:"user"`
+	Group        string        `json:"group", bson:"group"`
+	Permissions  string        `json:"permissions", bson:"permissions"`
+	CreatedDate  time.Time     `json:"createddate", bson:"createddate"`
+	LastModified time.Time     `json:"lastmodified", bson:"lastmodified"`
+	Type         string        `json:"type", bson:"type"`
+	Sub          []FileBrowser `json:"sub", bson:"sub"`
+}
+
+/*func Construct(line string, result []FileInfo) {
+	if !toolkit.IsPointer(result) {
+		e = errorlib.Error("", "", "Fetch", "Model object should be pointer")
+		return
+	}
+
+}*/
