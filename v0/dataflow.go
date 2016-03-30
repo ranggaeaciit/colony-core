d235f857149a5b4b0ed0fe8a4df7c903b555b7fe
frezadev <freza.dev@gmail.com>
2016-03-30 16:29:53 +0700

    add dataflow struct

--- v0/dataflow.go
+++ v0/dataflow.go
@@ -29,31 +29,41 @@ type DataFlow struct {
 }
 
 type FlowAction struct {
-	Id          string        `json:"_id"`
-	Name        string        `json:"name"`
-	Description string        `json:"description"`
-	Type        string        `json:"type"`
-	Server      Server        `json:"server"`
-	Action      interface{}   `json:"action"`
-	NextActions []interface{} `json:"nextactions"`
+	Id          string      `json:"_id"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Type        string      `json:"type"`
+	Server      Server      `json:"server"`
+	Action      interface{} `json:"action"`
+	OK          interface{} `json:"ok"`
+	KO          interface{} `json:"ko"`
 }
 
 type ActionHive struct {
-	Id string `json:"_id"`
 	// other field will be different for each action type
 }
 
 type ActionHDFS struct {
-	Id string `json:"_id"`
 	// other field will be different for each action type
 }
 
 type ActionSpark struct {
-	Id string `json:"_id"`
+	// other field will be different for each action type
+}
+
+type ActionQuestion struct {
+	Question string `json:"question"`
+	Yes      string `json:"yes"`
+	No       string `json:"no"`
 	// other field will be different for each action type
 }
 
 type ActionDecision struct {
-	Id string `json:"_id"`
+	Conditions []Conditions `json:"conditions"`
 	// other field will be different for each action type
+}
+
+type Condition struct {
+	Result     string `json:"result"`
+	FlowAction string `json:"flowaction"`
 }
