4766c19304c4c1fb30eaeec618662618e413b390
andrifebrih <andrifebrih@gmail.com>
2016-04-01 20:54:04 +0700

    add menu.go

---
+++ v0/menu.go
@@ -0,0 +1,23 @@
+package colonycore
+
+import (
+	"github.com/eaciit/orm/v1"
+	_ "github.com/eaciit/toolkit"
+)
+
+func (mn *Menu) TableName() string {
+	return "menus"
+}
+
+func (mn *Menu) RecordID() interface{} {
+	return mn.ID
+}
+
+type Menu struct {
+	orm.ModelBase
+	ID        string  `json:"_id",bson:"_id"`
+	AccessId  string  `json:"accessId",bson:"accessId"`
+	Title     string  `json:"title",bson:"title"`
+	Childrens []*Menu `json:"childrens",bson:"childrens"`
+	Link      string  `json:"link",bson:"link"`
+}
