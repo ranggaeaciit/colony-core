045de29288824679abf6358399055ea535fcf588
novalagung <caknopal@gmail.com>
2016-04-04 09:21:29 +0700

    no message

---
+++ v0/menu.go
@@ -0,0 +1,23 @@
+package colonycore
+
+import (
+	"github.com/eaciit/orm/v1"
+)
+
+type Menu struct {
+	orm.ModelBase
+	ID string `json:"_id",bson:"_id"`
+
+	AccessId  string `json:"AccessId",bson:"AccessId"`
+	Title     string `json:"title",bson:"title"`
+	Childrens []Menu `json:"childrens",bson:"childrens"`
+	Link      string `json:"link",bson:"link"`
+}
+
+func (a *Menu) TableName() string {
+	return "menu"
+}
+
+func (a *Menu) RecordID() interface{} {
+	return a.ID
+}
