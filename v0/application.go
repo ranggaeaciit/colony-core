4e7a06dca2ad54ed2bdbc1e9671642cc59694e98
novalagung <caknopal@gmail.com>
2016-04-08 17:09:05 +0700

    no message

--- v0/application.go
+++ v0/application.go
@@ -2,13 +2,13 @@ package colonycore
 
 import (
 	"github.com/eaciit/orm/v1"
+	"strings"
 )
 
 type Application struct {
 	orm.ModelBase
 	ID            string `json:"_id",bson:"_id"`
 	IsInternalApp bool
-	RunScript     map[string]string
 	AppsName      string
 	Port          string // for web type
 	Type          string
@@ -44,3 +44,36 @@ type TreeSourceModel struct {
 	Path      string             `json:"path"`
 	Items     []*TreeSourceModel `json:"items"`
 }
+
+func (a *Application) GetCommand(cmdName string) (bool, string) {
+	cmdString := ""
+
+	for _, raw := range a.Command.([]interface{}) {
+		each := raw.(map[string]interface{})
+		if each["key"].(string) == cmdName {
+			cmdString = each["value"].(string)
+			break
+		}
+	}
+
+	for _, raw := range a.Variable.([]interface{}) {
+		each := raw.(map[string]interface{})
+		if strings.Contains(cmdString, each["key"].(string)) {
+			cmdString = strings.Replace(cmdString, each["key"].(string), each["value"].(string), -1)
+		}
+	}
+
+	if cmdString == "" {
+		return false, ""
+	}
+
+	return true, cmdString
+}
+
+const (
+	App_Command_Start         = "start"
+	App_Command_Stop          = "stop"
+	App_Command_RunningStatus = "running_status"
+	App_Command_DeployStatus  = "deploy_status"
+	App_Variable_BinaryName   = "BINARY_NAME"
+)
