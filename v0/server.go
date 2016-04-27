4f086928f7c3f2075b4288ac4e53dba03c4f052c
andrifebrih <andrifebrih@gmail.com>
2016-04-27 11:19:28 +0700

    delete host

--- v0/server.go
+++ v0/server.go
@@ -18,9 +18,9 @@ import (
 
 type Server struct {
 	orm.ModelBase
-	ID   string `json:"_id", bson:"_id"`
-	OS   string `json:"os", bson:"os"`
-	Host string `json:"host", bson:"host"`
+	ID string `json:"_id", bson:"_id"`
+	OS string `json:"os", bson:"os"`
+	// Host string `json:"host", bson:"host"`
 
 	AppPath  string `json:"appPath", bson:"appPath"`
 	DataPath string `json:"dataPath", bson:"dataPath"`
